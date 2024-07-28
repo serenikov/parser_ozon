141c9a7a9c27e1c0a58734e0086760e6a49cddde
serenikov <fanat884@rambler.ru>
2024-07-28 13:13:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import time as tm
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 # Загрузка кодов товаров из файла
 with open('codes.txt', 'r') as f:
