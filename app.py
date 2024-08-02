e76b6448ab30569ef37ae52ff9fae7f84618e33b
serenikov <fanat884@rambler.ru>
2024-08-02 03:44:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,18 +24,15 @@ df = pd.DataFrame()
 
 #driver = uc.Chrome(executable_path='C:/path/to/chromedriver.exe')
 uc.TARGET_VERSION = 127
-driver = uc.Chrome()
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--enable-javascript')
-chrome_options.add_argument('--disable-gpu')
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15'
-chrome_options.add_argument('User-Agent={0}'.format(user_agent))
-chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-chrome_options.add_experimental_option('useAutomationExtension', True)
+chromedriver_path = '/Users/altblog/Downloads/chromedriver-mac-x64/chromedriver'
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-driver = uc.Chrome(executable_path='/path_to_your_driver/chromedriver',chrome_options=chrome_options,service_args=['--quiet'])
+# Create a Service instance for chromedriver
+service = Service(chromedriver_path)
+
+# Create a WebDriver instance using the service and options
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.implicitly_wait(10)
 
