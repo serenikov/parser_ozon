5025de893e76b66ed6b89ada7393604ce939fb90
serenikov <fanat884@rambler.ru>
2024-08-02 05:08:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ from selenium.webdriver.chrome.service import Service
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+#my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
 options = uc.ChromeOptions()
 options.add_argument("--headless")
-options.add_argument(f"user-agent={my_user_agent}")
+#options.add_argument(f"user-agent={my_user_agent}")
  
 # Initialize Chrome WebDriver with the specified options
-uc.TARGET_VERSION = 127
-driver = uc.Chrome(browser_executable_path="/usr/bin/google-chrome-beta")
+#uc.TARGET_VERSION = 127
+driver = uc.Chrome(options=options)
 
 
 # Загрузка кодов товаров из файла
