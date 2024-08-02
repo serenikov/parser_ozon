cb17fd1617e14e9a32dc6b35968fc871dd65eeae
serenikov <fanat884@rambler.ru>
2024-08-02 05:39:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ import webdriver_manager
 
 from bs4 import BeautifulSoup
 import time as tm
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.service import Service
 
 from webdriver_manager.chrome import ChromeDriverManager
 
