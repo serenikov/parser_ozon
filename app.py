86dceb51b1a4e8b028ffe380a9e5d3aded1f8c6c
serenikov <fanat884@rambler.ru>
2024-08-07 22:21:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,21 +14,24 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import undetected_chromedriver as uc
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = uc.ChromeOptions()
 options.add_argument("--headless")
  
 # Initialize Chrome WebDriver with the specified options
 
-caps = DesiredCapabilities.CHROME
-caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+#caps = DesiredCapabilities.CHROME
+#caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
 #options = Options()
 options.add_argument('--deny-permission-prompts')
 options.add_argument("--disable-notifications")  
 options.add_argument('--blink-settings=imagesEnabled=false')
-driver = uc.Chrome(headless=True, use_subprocess=False, options=options, version_main=127, desired_capabilities=caps)
+
+driver = uc.Chrome(headless=True, use_subprocess=False, service=Service(ChromeDriverManager().install()), options=options, version_main=127)
 
 driver.implicitly_wait(10)
 
