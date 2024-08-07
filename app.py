564199d7f961108df1a826d564c4d72ba0db6bc5
serenikov <fanat884@rambler.ru>
2024-08-07 21:44:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import webdriver_manager
 from bs4 import BeautifulSoup
 import time as tm
 #from selenium import webdriver
-import undetected_chromedriver as uc
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -17,6 +17,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+import undetected_chromedriver as uc
 
 
 
