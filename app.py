60b0d9d6b0b04fc6d7634d87f7189dc98aca2a61
serenikov <fanat884@rambler.ru>
2024-08-09 22:03:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ page_source = str(driver.page_source)
 soup = BeautifulSoup(page_source, 'html.parser')
         # работа с html
         # Получение названия товара
-print(soup[: 1000])
+print(soup[:1000])
 print(soup[1000:])
 name_element = soup.find('h1')
 name = name_element.text.strip().replace('"', "&quot;")
