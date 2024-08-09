91181968721b068d1c66eeee91924340341fb052
serenikov <fanat884@rambler.ru>
2024-08-10 02:59:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ chrome_options = Options()
 #chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/home/<user>/chromedriver',chrome_options=chrome_options)
+driver = webdriver.Chrome('/home/<user>/chromedriver',options=chrome_options)
 
 driver.implicitly_wait(5)
 
