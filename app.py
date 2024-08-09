f6af2fc920005d7b94679b623005bec55e95a0da
serenikov <fanat884@rambler.ru>
2024-08-10 01:44:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ tm.sleep(20)
 #driver.find_element(By.CLASS_NAME, 'rb').click()
 #table_button = driver.find_element(By.XPATH, "//button[@class='rb']")
 #driver.execute_script('arguments[0].click();', table_button)
+driver.set_window_size(1440, 1024)
 table_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, 'rb')))
 table_button.click()
  
