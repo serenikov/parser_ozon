040c5d371d2b0b490604f57678f8ee34a8941ca6
serenikov <fanat884@rambler.ru>
2024-08-10 00:14:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,14 @@ url = 'https://www.ozon.ru/product/skoba-stroitelnaya-200-mm-x-8-mm-50-sht-87612
 driver.get(url)
 tm.sleep(20)
 #driver.find_element(By.CLASS_NAME, 'rb').click()
-driver.find_element(By.XPATH, "//button[@class='rb']").sendKeys(Keys.ENTER)
+table_button = driver.find_element(By.XPATH, "//button[@class='rb']")
+
+try:
+    WebDriverWait(driver, 15).until(EC.element_to_be_clickable(table_button)).click()
+except WebDriverException as e:
+    print('failed')
+    print(e)
+ 
 tm.sleep(20)
 #logs = driver.get_log('performance')
 #status_code = status_code_first_request(logs)
