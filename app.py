8ae894020ddd529fea7187ef75443c09baf4deb6
serenikov <fanat884@rambler.ru>
2024-08-11 02:02:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from gspread import Client, Spreadsheet, Worksheet, service_account, exceptions
 
 json = os.getenv('GOOGLE_SHEETS_API')
 gs = service_account(filename='google-services.json')
-
+wks = gs.open("parstest").sheet1
+wks.update('A1', [[1,2], [3,4]])
      
 url = 'https://www.ozon.ru/product/skoba-stroitelnaya-200-mm-x-8-mm-50-sht-876124103'
 
