0f3fb24e5a099899a45c4f1e84bf5eeeffce726d
serenikov <fanat884@rambler.ru>
2024-08-10 19:40:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -81,9 +81,7 @@ tm.sleep(20)
 button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/button")))
 action = ActionChains(driver)
 action.move_to_element(button).move_by_offset(0, 0).click().perform()
-
-Actions actionObject = new Actions(driver);
-actionObject.keyDown(Keys.CONTROL).sendKeys(Keys.F5).keyUp(Keys.CONTROL).perform‌​()
+driver.navigate().refresh()
 
 
 #document.querySelector("body > div > div > div.bc > button")
