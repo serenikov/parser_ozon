dc701dfe0a0d28a9d3aadb421bb52e0cb375da02
serenikov <fanat884@rambler.ru>
2024-08-11 00:15:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,13 @@ import os
 import telebot
 import pandas as pd
 import requests
-
 from bs4 import BeautifulSoup
 import time as tm
-#from selenium import webdriver
-from seleniumbase import Driver
 
-
-driver = Driver(uc=True, headless=True)
-# Initialize Chrome WebDriver with the specified options         
+     
 url = 'https://www.ozon.ru/product/skoba-stroitelnaya-200-mm-x-8-mm-50-sht-876124103'
 
-apikey = 'db752e79d9168c1f74c544ca5fff335b1ee294c1'
+apikey = os.getenv('ZENROW_API_KEY')
 print(apikey)
 params = {
     'url': url,
@@ -22,46 +17,52 @@ params = {
 	'premium_proxy': 'true',
 }
 response = requests.get('https://api.zenrows.com/v1/', params=params)
-print(response.text)
 
-        # Создание объекта BeautifulSoup для парсинга HTML-кода
+# Создание объекта BeautifulSoup для парсинга HTML-кода
 soup = BeautifulSoup(response.text, 'html.parser')
-        # работа с html
-        # Получение названия товара
-print(soup)
+
+# работа с html
+ # Получение названия товара
 
 name_element = soup.find('h1')
 name = name_element.text.strip().replace('"', "&quot;")
 
 # Получение цены со скидкой без Ozon Карты
 try:
-    price_element = soup.find('span', string="без Ozon Карты").parent.parent.find('div').findAll('span')
-    discount_price = price_element[0].text.strip() if price_element[0] else ''
+	price_element = soup.find('span', string="без Ozon Карты").parent.parent.find('div').findAll('span')
+	discount_price = price_element[0].text.strip() if price_element[0] else ''
+	#<span class="mn6_27 m6n_27 mo_27">1 542 ₽</span>
+	discount_price.replace("₽","")
+	discount_price.replace(" ","")
 except:
-    discount_price = 0
+	discount_price = 0
 
 # Получение цены базовая
 try:
-    base_price = price_element[1].text.strip() if price_element[1] is not None else ''
+	base_price = price_element[1].text.strip() if price_element[1] is not None else ''
+	base_price.replace("₽","")
+	base_price.replace(" ","")
 except:
-    base_price = 0
+	base_price = 0
 
 # Получение цены по Ozon Карте
 try:
-    ozon_card_price_element = soup.find('span', string="c Ozon Картой").parent.find('div').find('span')
-    ozon_card_price = ozon_card_price_element.text.strip() if ozon_card_price_element else ''
+	ozon_card_price_element = soup.find('span', string="c Ozon Картой").parent.find('div').find('span')
+	ozon_card_price = ozon_card_price_element.text.strip() if ozon_card_price_element else ''
+	ozon_card_price.replace("₽","")
+	ozon_card_price.replace(" ","")
 except:
     ozon_card_price = 0
 
  # Получение продавца
-#seller_element = soup.find('div', {"data-widget":"webCurrentSeller"}).select('a[href*="ozon.ru/seller"]' )
-#seller = seller_element[-1].get('title').strip() if seller_element else ''
+seller_element = soup.find('div', {"data-widget":"webCurrentSeller"}).select('a[href*="ozon.ru/seller"]' )
+seller = seller_element[-1].get('title').strip() if seller_element else ''
 
 print(name)
 print('Цена без Озон-карты: ' + str(discount_price))
 print('Базовая цена: ' + str(base_price))
 print('Цена с Озон-карты: ' + str(ozon_card_price))
-#print('Продавец: ' + seller)
+print('Продавец: ' + seller)
 
 # Загрузка кодов товаров из файла
 #TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
