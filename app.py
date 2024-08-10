0b0e7ca4e0c1add539ef14d5b2a802a4c47f7186
serenikov <fanat884@rambler.ru>
2024-08-10 03:35:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -67,12 +67,14 @@ tm.sleep(20)
 #action.click(on_element = table_button)
 #action.perform()
 
-button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/button")))
-button.click()
+button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/button")))
+action = ActionChains(driver)
+action.move_to_element(button).move_by_offset(0, 0).click().perform()
+#button.click()
 
  
-tm.sleep(20)
-driver.implicitly_wait(20)
+tm.sleep(10)
+driver.implicitly_wait(10)
 #logs = driver.get_log('performance')
 #status_code = status_code_first_request(logs)
 page_source = str(driver.page_source)
