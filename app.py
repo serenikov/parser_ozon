01cf7613b5a30838260b48a87852b1ca5f02ba2a
serenikov <fanat884@rambler.ru>
2024-08-10 21:46:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,61 +7,10 @@ import webdriver_manager
 from bs4 import BeautifulSoup
 import time as tm
 #from selenium import webdriver
+from seleniumbase import Driver
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import undetected_chromedriver as uc
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.action_chains import ActionChains
-#from undetected_chromedriver import Chrome
-
-options = uc.ChromeOptions()
-options.add_argument("--headless")
-#options.add_argument('--auto-open-devtools-for-tabs')
-#options = ChromeOptions()
-#my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
-#options.add_argument(f"user-agent={my_user_agent}")
-#options.add_argument("--headless") # Runs Chrome in headless mode.
-#options.add_argument('--no-sandbox') # # Bypass OS security model
-options.add_argument('start-maximized')
-#options.add_argument('disable-infobars')
-#options.add_argument("--disable-extensions")
-
-
-# Initialize Chrome WebDriver with the specified options
-
-#caps = DesiredCapabilities.CHROME
-#caps['goog:loggingPrefs'] = {'performance': 'ALL'}
-#options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
-#options = Options()
-options.add_argument('--deny-permission-prompts')
-options.add_argument("--disable-notifications")  
-#options.add_argument('--blink-settings=imagesEnabled=false')
-
-driver = uc.Chrome(headless=True, use_subprocess=False, service=Service(ChromeDriverManager().install()), options=options, version_main=127)
-#driver = uc.Chrome(headless=False, use_subprocess=True, service=Service(ChromeDriverManager().install()), options=options, version_main=127)
-#driver = Chrome(options=options)
-
-driver.implicitly_wait(5)
-
-#driver = uc.Chrome(options=options)
-
-#def status_code_first_request(performance_log):
-#    for line in performance_log:     
-#        try:
-#            print(line)
-#            json_log = json.loads(line['message'])
-#            if json_log['message']['method'] == 'Network.responseReceived':
-#                print('есть лог, статус: ' + json_log['message']['params']['response']['status'])
-#                return json_log['message']['params']['response']['status']
-#        except:
-#            pass
-         
+driver = Driver(uc=True, headless=True)
+# Initialize Chrome WebDriver with the specified options         
 url = 'https://www.ozon.ru/product/skoba-stroitelnaya-200-mm-x-8-mm-50-sht-876124103/'
 
 # Загрузка страницы товара с помощью веб-драйвера
