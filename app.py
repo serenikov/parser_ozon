b4d79ba6220eb329c146e06c1365d1e167bb9e9f
serenikov <fanat884@rambler.ru>
2024-08-11 05:12:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info,
 
 # Авторизация
 client = gspread.authorize(credentials)
-sheet = client.open('скобы конкуренты (цены)').sheet7
+sheet = client.open('скобы конкуренты (цены)').get_worksheet(7)
+
 #sheet.update_acell('A1', 'Hello, World!')
      
 url_1 = 'https://www.ozon.ru/product/'
